dc21ff1deb13c3799170cdc997bd3bd7de2d63a9
Alejandro Vera <a.vera07@ufromail.cl>
2020-12-20 21:22:25 -0300

    Implementando gastos

--- src/main/java/Gasto.java
+++ src/main/java/Gasto.java
@@ -1,2 +1,11 @@
 public class Gasto {
+    private String nombre;
+    private int precio;
+    private boolean pagado;
+
+    public Gasto(String nombre, int precio, boolean pagado) {
+        this.nombre = nombre;
+        this.precio = precio;
+        this.pagado = pagado;
+    }
 }
